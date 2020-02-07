2208993b5fdc0f3705a182dcd1b07cb5cd025037
Administrator <Administrator@WIN-TULDIJKCOT4>
2020-02-07 15:58:31 +0800

    222

--- Test2/src/test2.java
+++ Test2/src/test2.java
@@ -1,10 +1,12 @@
 
 public class test2 {
-<<<<<<< HEAD
+
 //44444444444444
 	//999999999999999
-=======
+public static void main(String[] args) {
+	
+}
 //4444444444444444
   //6666666
->>>>>>> branch 'mester' of https://github.com/542269226/xiaoge.git
+
 }

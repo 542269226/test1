24e692060ed47aa5332ebdfb94a4e934acbd7553
Administrator <Administrator@WIN-TULDIJKCOT4>
2020-02-07 16:17:54 +0800

    ry

--- Test2/src/test2.java
+++ Test2/src/test2.java
@@ -1,10 +1,8 @@
 
 public class test2 {
-<<<<<<< HEAD
 //44444444444444
 	//999999999999999
-=======
 //4444444444444444
   //6666666
->>>>>>> branch 'mester' of https://github.com/542269226/xiaoge.git
+
 }
